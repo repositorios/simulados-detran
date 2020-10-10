31cc43a8fa59c81e9048b9dea658269f15f6f330
Renato Tavares <dr.renatotavares@gmail.com>
2020-10-09 21:03:28 -0300

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/org/pacote/android/apps/simuladodetran/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/org/pacote/android/apps/simuladodetran/ExampleInstrumentedTest.java
@@ -19,7 +19,7 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
-        // Context of the app under test.
+      
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("org.pacote.android.apps.simuladodetran", appContext.getPackageName());
